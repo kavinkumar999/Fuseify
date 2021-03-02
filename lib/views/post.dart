b20807a8d961ae0ee6de50855485fc2a43163b41
kavin <kavinkumarnkm007@gmail.com>
2021-03-02 23:29:57 +0530

    feat[post]: improve post page

--- lib/views/post.dart
+++ lib/views/post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fuseify/utils/Colors.dart';
 import 'package:fuseify/utils/common.dart';
 
 class Post extends StatefulWidget {
@@ -9,13 +10,58 @@ class Post extends StatefulWidget {
 class _PostState extends State<Post> {
   @override
   Widget build(BuildContext context) {
-    return PageScaffold(
-      home: true,
-      title: 'Post it',
-      subtitle: 'Social dashboard',
+    return Scaffold(
       backgroundColor: Colors.white,
-
-      
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(padding, 30, padding, 6),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  InkWell(
+                    child: Icon(Icons.chevron_left,size: 50,)
+                  ),
+                    
+                  InkWell(
+                    child: Icon(Icons.cancel,size: 50,)
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  SizedBox(height: 40),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: padding),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Create a Post",
+                          style: TextStyle(
+                            fontSize: 38,
+                            color: UIColors.navyBlue,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 1.2,
+                          ),
+                        ),
+                        
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
